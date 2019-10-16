78bbcf48675a81374d5397f056045be6814be0d1
João Diego <jdflorencio@gmail.com>
2019-10-16 01:37:43 -0300

    estrutura inicial

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import angular from 'angular';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export const app = 'app'
+
+angular.module(app, [
+    // 'ngResource',
+    // ClienteModule,
+  ])
