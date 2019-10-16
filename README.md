6188ade66afe800d149c409928b5494ae36a65df
João Diego <jdflorencio@gmail.com>
2019-10-16 01:55:04 -0300

    organizando

---
+++ README.md
@@ -0,0 +1,16 @@
+# FRONT-ERP
+Uma Aplicação desenvolvida com Angularjs + Webpack + Bootstrap
+
+## Comandos principais
+
+executando o servidor de desenvolvimento: 
+
+```
+npm run serve
+```
+
+
+gerando o build: 
+```
+npx webpack -mode none
+```
--- src/index.js
+++ src/index.js
@@ -4,6 +4,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 export const app = 'app'
 
 angular.module(app, [
+    'ngRoute',
+
     // 'ngResource',
     // ClienteModule,
   ])
+
+  .config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+    $locationProvider.hashPrefix('!');
+  
+    $routeProvider.otherwise({redirectTo: '/view1'});
+  }]);
+  
